efe156157f8869e816994ef156a33b5c892d6a93
Sanjay Baskaran <sanjaysmack@gmail.com>
2024-08-04 01:41:06 +0530

    useEffect Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import react, { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
+const [counter, setCounter] = useState(0);
+const [todo, setTodo] = useState([{
+  "id":3,"title":"Todo 3","description":"This is todo 3","completed":false
+},{
+  "id":4,"title":"Todo 4","description":"This is todo 4","completed":false}
+]);
 
-  return (<div>
-  <WrapperComponent>Hi There!</WrapperComponent>
-  <br></br>
-  <WrapperComponent>Hello Back!</WrapperComponent>
+useEffect(()=>{
+  fetch("https://sum-server.100xdevs.com/todos")
+  .then(async (res) =>{
+    const json = await res.json();
+    setTodo(json.todos);
+  })
+},[counter])
+
+return (
+  <div>
+    <button onClick={()=>{
+      setCounter(counter+1);
+    }}>Counter</button>
+    {todo.map(todo => {return <div key={todo.id}><p> title={todo.title} description={todo.description}</p></div>} )}
   </div>
-  )
-}
-
-function WrapperComponent({children}){
-return (<div style={{border: '2px solid black'}}>
-  {children}
-</div>)
+)
 }
 
 export default App
