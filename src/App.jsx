dd809f1367bfe51e7e96af3c23244aab240cf506
Sanjay Baskaran <sanjaysmack@gmail.com>
2024-08-03 23:40:37 +0530

    wrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,19 @@
 import react, { useState } from 'react'
 
 function App() {
-  const [todo,setTodo] = useState([{
-    id:1,
-    title: "Title1",
-    description: "Description1"
-  },{
-    id:2,
-    title: "Title2",
-    description: "Description2"
-  },{
-    id:3,
-    title: "Title3",
-    description: "Description3"
-  }
-])
 
-function addTodo(){
-  setTodo([...todo,{
-    id:4,
-    title: "title4",
-    description: "description4"
-  }])
-}
-  return (
-    <div>
-      <TextWithButton />
-      <Text text={"Text Component"} />
-      <button onClick={addTodo}>Add Todo</button>
-      {todo.map(todo => {return <Todo key={todo.id} title={todo.title} description={todo.description}></Todo>} )}
-    </div>
+  return (<div>
+  <WrapperComponent>Hi There!</WrapperComponent>
+  <br></br>
+  <WrapperComponent>Hello Back!</WrapperComponent>
+  </div>
   )
 }
 
-function TextWithButton(){
-  const [count,setCount] = react.useState(0);
-
-  function updateTitle(){
-    setCount("TextWithButton "+Math.random());
-  }
-
-  return <div>
-    <button onClick={updateTitle}>Update the Component</button>
-    <Text text={count} />
-    </div>
-}
-function Text(props){
-  return <div>{props.text}</div>
-}
-
-function Todo(props){
-  return <div>
-    <h1>{props.title}</h1>
-    <h2>{props.description}</h2>
-  </div>
+function WrapperComponent({children}){
+return (<div style={{border: '2px solid black'}}>
+  {children}
+</div>)
 }
 
 export default App
