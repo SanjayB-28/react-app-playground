871cb2152d96430c8b86670bb75d5d95bded7b5a
Sanjay Baskaran <sanjaysmack@gmail.com>
2024-08-04 02:17:31 +0530

    use effect assignment update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@ import { useEffect, useState } from 'react'
 
 function App() {
 const [counter, setCounter] = useState(0);
-const [todo, setTodo] = useState([{
-  "id":3,"title":"Todo 3","description":"This is todo 3","completed":false
-},{
-  "id":4,"title":"Todo 4","description":"This is todo 4","completed":false}
-]);
+const [todo, setTodo] = useState([]);
 
 useEffect(()=>{
+  console.log(counter+" inside use");
   fetch("https://sum-server.100xdevs.com/todos")
   .then(async (res) =>{
     const json = await res.json();
@@ -18,10 +15,12 @@ useEffect(()=>{
 
 return (
   <div>
+    {console.log(counter+" inside app")}
     <button onClick={()=>{
       setCounter(counter+1);
     }}>Counter</button>
-    {todo.map(todo => {return <div key={todo.id}><p> title={todo.title} description={todo.description}</p></div>} )}
+    {todo.map(todo => {return <div key={todo.id}><p> title={todo.title} description={todo.description}
+      </p></div>} )}
   </div>
 )
 }
